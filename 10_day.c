8417de4d575ce365a1f090ba54aa60b988a0e1a0
viditahuja <vvvvvidit@gmail.com>
2026-02-11 16:18:26 +0530

    Add files via upload

---
+++ 10_day.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+
+    int left = 0;
+    int right = strlen(s) - 1;
+    int isPalindrome = 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            isPalindrome = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (isPalindrome)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
